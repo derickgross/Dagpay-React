107a19d8bc0871d627998d8a7615d793fd123303
Derick Gross <derick.gross@gmail.com>
2019-05-24 14:47:30 -0400

    hide employee dependents by default, add event listeners to display on click/enter; style; begin converting form inputs to controlled components

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Dependent from '../Dependent/Dependent'
+import "./Employee.css"
 
 const Employee = (props) => {
+	let dependentsDeductions = 0;
+
 	const dependents = props.dependents.map(dependent => {
+		dependentsDeductions += Number.parseFloat(dependent.deduction.toFixed(2));
+		
 		return (
 			<Dependent
-				key={1}
+				key={`${dependent.firstname}-${dependent.lastname}`}
 				employeeId={props.employeeid}
 				firstName={dependent.firstname}
 				lastName={dependent.lastname}
@@ -16,13 +21,13 @@ const Employee = (props) => {
 
 	return (
 		<li 
-			key={props.employeeid}
-			id={`employee-${props.employeeid}`}
+			key={props.employeeId}
+			id={`employee-${props.employeeId}`}
 			className="employee"
 			tabIndex="0"
 		>
 			{`${props.firstName} ${props.lastName}: `}
-			<span className="money">{`$${props.deduction}`}</span>
+			<span className="money">{`$${props.deduction}`}</span>{dependentsDeductions > 0 ? <span className="money">{` (+$${dependentsDeductions.toFixed(2)})`}</span> : '' }
 			<ul>
 				{dependents}
 			</ul>
